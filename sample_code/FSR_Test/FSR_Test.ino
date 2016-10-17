a8b245c93b56c9bb41050fadb04d2d94095178e0
jazzij <jazzij@umich.edu>
2016-10-16 23:36:38 -0400

    updated FSR_test

--- sample_code/FSR_Test/FSR_Test.ino
+++ sample_code/FSR_Test/FSR_Test.ino
@@ -1,15 +1,12 @@
 /*
  * FSR testing sketch
- * Pin A0 is analog data in
+ * Pin A3 is analog data in
  */
 
-//int fsrAPin = A1;
-//int fsrBPin = A2;
-int fsrCPin = A3;
+int fsrPin = A3;
 
-//int fsrA_Reading = 0;
-//int fsrB_Reading = 0;
-int fsrC_Reading = 0;
+
+int fsr_Reading = 0;
 
 int fsrLastRead = 0;
 int MIN_FSR = 0;
@@ -20,22 +17,16 @@ int MAX_FSR = 1023;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //pinMode(fsrAPin, INPUT);
-  //pinMode(fsrBPin, INPUT);
-  pinMode(fsrCPin, INPUT);  
+
+  pinMode(fsrPin, INPUT);  
 
 }
 
 void loop() {
-  //fsrA_Reading = analogRead(fsrAPin);
-  //fsrB_Reading = analogRead(fsrBPin);
-  fsrC_Reading = analogRead(fsrCPin);
 
-//  Serial.print("FSR1: ");
-//  Serial.println(fsrA_Reading);
-//  Serial.print("FSR2: ");
-//  Serial.println(fsrB_Reading);
-//  Serial.print("FSR3: ");
+  fsr_Reading = analogRead(fsrCPin);
+
+  Serial.print("FSR: ");
   Serial.println(fsrC_Reading);
 
 
